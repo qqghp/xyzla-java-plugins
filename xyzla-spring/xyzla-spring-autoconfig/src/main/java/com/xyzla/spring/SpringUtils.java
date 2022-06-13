3b9b2bd883369dd1966adac6f68b2a8ab0426c20
ryan <xqghp@126.com>
2022-06-13 10:27:17 +0800

    add xyzla-spring

--- xyzla-spring/xyzla-spring-autoconfig/src/main/java/com/xyzla/spring/SpringUtils.java
+++ xyzla-spring/xyzla-spring-autoconfig/src/main/java/com/xyzla/spring/SpringUtils.java
@@ -1,13 +1,13 @@
-package com.xyzla.common.component;
+package com.xyzla.spring;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
-@Component
 public class SpringUtils implements ApplicationContextAware {
 
     private static final Logger logger = LoggerFactory.getLogger(SpringUtils.class);
@@ -23,29 +23,58 @@ public class SpringUtils implements ApplicationContextAware {
         logger.info("ApplicationContext 配置成功, 在普通类可以通过调用 SpringUtils.getApplicationContext() 获取 applicationContext 对象, applicationContext = {}", SpringUtils.applicationContext);
     }
 
-    // 获取 applicationContext
+    /**
+     * 获取 applicationContext
+     *
+     * @return
+     */
     public static ApplicationContext getApplicationContext() {
         return applicationContext;
     }
 
-    // 通过 name 获取 Bean
+    /**
+     * 通过 name 获取 Bean
+     *
+     * @param name
+     * @return
+     */
     public static Object getBean(String name) {
         return getApplicationContext().getBean(name);
     }
 
-    // 通过 class 获取 Bean
+    /**
+     * 通过 class 获取 Bean
+     *
+     * @param clazz
+     * @param <T>
+     * @return
+     */
     public static <T> T getBean(Class<T> clazz) {
         return getApplicationContext().getBean(clazz);
     }
 
-    // 通过 name,以及 Clazz 返回指定的 Bean
+    /**
+     * 通过 name,以及 Clazz 返回指定的 Bean
+     *
+     * @param name
+     * @param clazz
+     * @param <T>
+     * @return
+     */
     public static <T> T getBean(String name, Class<T> clazz) {
         return getApplicationContext().getBean(name, clazz);
     }
 
-
-    // 获取当前环境
+    /**
+     * 通过 name,以及 Clazz 返回指定的 Bean
+     *
+     * @return
+     */
     public static String getActiveProfile() {
         return applicationContext.getEnvironment().getActiveProfiles()[0];
+    }
+
+    public static String getProperty(String key) {
+        return applicationContext.getEnvironment().getProperty(key);
     }
 }
